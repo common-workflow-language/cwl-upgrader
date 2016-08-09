ffb8c9a6d478a3df03b3ac1aeb966b4a2792d52f
Michael R. Crusoe <michael.crusoe@gmail.com>
2016-08-09 20:33:37 +0300

    now shortens types, when possible

--- cwlupgrader/main.py
+++ cwlupgrader/main.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 import ruamel.yaml
 from typing import Any, Dict
-from collections import MutableMapping
+from collections import Mapping, Sequence
 import sys
+import copy
 
 def main():  # type: () -> int
     for path in sys.argv[1:]:
@@ -49,7 +50,7 @@ def _draft3_to_v1_0(document):
         del document["description"]
 
     for key, value in document.items():
-        if isinstance(value, MutableMapping):
+        if isinstance(value, Mapping):
             document[key] = _draft3_to_v1_0(value)
 
     return document
@@ -58,13 +59,34 @@ def setupCLTMappings(document):  # type: (Dict[unicode, Any]) -> None
     for paramType in ['inputs', 'outputs']:
         params = {}
         for param in document[paramType]:
+            paramID = param['id'].lstrip('#')
+            param['type'] = shortenType(param['type'])
             if len(param) == 2 and 'type' in param:
-                params[param['id']] = param['type']
+                params[paramID] = param['type']
             else:
-                paramID = param['id']
                 del param['id']
                 params[paramID] = param
         document[paramType] = params
+
+def shortenType(typeObj):
+    # type: (List[Any]) -> Union[Unicode, List[Any]
+    if isinstance(typeObj, (str, unicode)) or not isinstance(typeObj, Sequence):
+        return typeObj
+    newType = []
+    for entry in typeObj:  # find arrays that we can shorten and do so
+        if isinstance(entry, Mapping):
+            if (entry['type'] == 'array' and
+                    isinstance(entry['items'], (str, unicode))):
+                entry = entry['items'] + '[]'
+        newType.extend([entry])
+    typeObj = newType
+    if len(typeObj) == 2:
+        if 'null' in typeObj:
+            typeCopy = copy.deepcopy(typeObj)
+            typeCopy.remove('null')
+            if isinstance(typeCopy[0], (str, unicode)):
+                return typeCopy[0] + '?'
+    return typeObj
 
 if __name__ == "__main__":
     sys.exit(main())
