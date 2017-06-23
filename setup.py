1ab37b159e873461d1283126a1f047b529f31d5b
requires.io <support@requires.io>
2017-06-23 12:51:21 +0300

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='cwl-upgrader',
       license='Apache 2.0',
       packages=["cwlupgrader"],
       install_requires=[
-          'ruamel.yaml==0.15.9',
+          'ruamel.yaml==0.15.10',
           'typing' ],
       entry_points={
           'console_scripts': [ "cwl-upgrader = cwlupgrader.main:main" ]
