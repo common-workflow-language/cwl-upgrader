0673367944d23bad7f50683e5528a400cf1879ba
Tomoya Tanjo <ttanjo@gmail.com>
2021-11-12 10:35:13 +0000

    Move data for test to `testdata`

--- tests/test_complete.py
+++ tests/test_complete.py
@@ -8,9 +8,9 @@ from .util import get_data
 
 def test_draft3_workflow(tmp_path: Path) -> None:
     """Basic draft3 to CWL v1.1 test."""
-    main([f"--dir={tmp_path}", "--v1-only", get_data("tests/draft-3-wf.cwl")])
+    main([f"--dir={tmp_path}", "--v1-only", get_data("testdata/draft-3/draft-3-wf.cwl")])
     result = filecmp.cmp(
-        get_data("tests/draft-3-wf-v1.0.cwl"),
+        get_data("testdata/v1.0/draft-3-wf-v1.0.cwl"),
         tmp_path / "draft-3-wf.cwl",
         shallow=False,
     )
