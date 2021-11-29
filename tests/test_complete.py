9c0f879866ce612c8245c8ec82fc97c58e785c3a
Peter Amstutz <peter.amstutz@curii.com>
2021-11-29 15:56:26 -0500

    make format

--- tests/test_complete.py
+++ tests/test_complete.py
@@ -75,7 +75,9 @@ def test_v1_1_to_v1_2(tmp_path: Path) -> None:
 
 def test_packed_graph(tmp_path: Path) -> None:
     """Test packed document with $graph."""
-    main([f"--dir={tmp_path}", "--v1.1-only", get_data("testdata/v1.0/conflict-wf.cwl")])
+    main(
+        [f"--dir={tmp_path}", "--v1.1-only", get_data("testdata/v1.0/conflict-wf.cwl")]
+    )
     assert filecmp.cmp(
         get_data("testdata/v1.1/conflict-wf.cwl"),
         tmp_path / "conflict-wf.cwl",
