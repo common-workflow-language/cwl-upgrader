cba3da54a5dc5adc2cefc519f22291c86615a840
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-05-02 09:37:49 +0200

    prepare for 1.2.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PYTEST_RUNNER = ["pytest-runner", "pytest-cov"] if NEEDS_PYTEST else []
 
 setup(
     name="cwl-upgrader",
-    version="1.2.4",
+    version="1.2.5",
     description="Common Workflow Language standalone document upgrader",
     long_description=open(README).read(),
     author="Common Workflow Language contributors",
